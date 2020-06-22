fe21d86d0b0e134cafc4446c66741db4393d1c64
aky15 <ankeyuthu@gmail.com>
2020-06-22 11:52:06 +0800

    Update model.py

--- egs/wsj/steps/model.py
+++ egs/wsj/steps/model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 import numpy as np
+import six
 import batchnormsync as bns
 
 
