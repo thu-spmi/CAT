f10396e42bde71cf4d3f60c55e0144e69c3bd00d
aky15 <ankeyuthu@gmail.com>
2020-08-03 18:22:28 +0800

    Update run.sh

--- egs/aishell/run.sh
+++ egs/aishell/run.sh
@@ -113,12 +113,13 @@ if [ $stage -le 5 ]; then
   copy-feats "$feats_test" "ark,scp:data/test_data/test.ark,data/test_data/test.scp"
 fi
 
-dir=exp/blstm
+arch=BLSTM
+dir=exp/$arch
 output_unit=$(awk '{if ($1 == "#0")print $2 - 1 ;}' data/lang_phn/tokens.txt)
 
 if [ $stage -le 6 ]; then
     echo "nn training."
-    python3 ctc-crf/train.py --arch=BLSTM --output_unit=$output_unit --lamb=0.01 --data_path $data/hdf5 $dir
+    python3 ctc-crf/train.py --arch=$arch --output_unit=$output_unit --lamb=0.01 --data_path $data/hdf5 $dir
 fi
 
 nj=20
