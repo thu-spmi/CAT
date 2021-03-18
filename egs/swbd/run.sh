a9c69bb6f76749dc1661ca557eb58cf2489fab8e
maxwellzh <maxwellzh@outlook.com>
2021-03-18 13:02:30 +0800

    rename install_tutorial to rm the meaningless space; add path_weight to egs/swbd/path.sh; script location error fixed in egs/swbd/run.sh; link errors fixed in egs/swbd/steps and egs/swbd/utils; add .gitignore file

--- egs/swbd/run.sh
+++ egs/swbd/run.sh
@@ -101,7 +101,7 @@ if [ $stage -le 3 ]; then
   cat data/train_nodup/text_number | sort -k 2 | uniq -f 1 > data/train_nodup/unique_text_number
   mkdir -p data/den_meta
   chain-est-phone-lm ark:data/train_nodup/unique_text_number data/den_meta/phone_lm.fst
-  utils/ctc_token_fst_corrected.py den data/lang_phn/tokens.txt | fstcompile | fstarcsort --sort_type=olabel > data/den_meta/T_den.fst
+  ctc-crf/ctc_token_fst_corrected.py den data/lang_phn/tokens.txt | fstcompile | fstarcsort --sort_type=olabel > data/den_meta/T_den.fst
   fstcompose data/den_meta/T_den.fst data/den_meta/phone_lm.fst > data/den_meta/den_lm.fst
   echo "prepare denominator finished"
  
