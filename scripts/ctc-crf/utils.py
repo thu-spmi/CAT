7461c09212b37aecdeadd82bf25ab0b2dd891026
ankeyu <ankeyuthu@gmail.com>
2021-04-27 19:12:49 +0800

    add func to move optimizer to specific hardware

--- scripts/ctc-crf/utils.py
+++ scripts/ctc-crf/utils.py
@@ -189,6 +189,19 @@ def adjust_lr_distribute(optimizer, origin_lr, lr, cv_loss, prev_cv_loss, epoch,
 
     return new_lr
 
+def optimizer_to(optim, device):
+    for param in optim.state.values():
+        # Not sure there are any global tensors in the state dict
+        if isinstance(param, torch.Tensor):
+            param.data = param.data.to(device)
+            if param._grad is not None:
+                param._grad.data = param._grad.data.to(device)
+        elif isinstance(param, dict):
+            for subparam in param.values():
+                if isinstance(subparam, torch.Tensor):
+                    subparam.data = subparam.data.to(device)
+                    if subparam._grad is not None:
+                        subparam._grad.data = subparam._grad.data.to(device)
 
 def parse_args():
     parser = argparse.ArgumentParser(description="recognition argument")
