[bug][reproducibility] fix a bug while setting seed; [improve][performance] better performance with gradient accumulation; [code] rename utils.py -> coreutils.py

scripts/ctc-crf/_layers.py
@@ -101,7 +101,9 @@ class Conv2dSubdampling(nn.Module):
         out = out.permute(0, 2, 1, 3)
         # [B, T//4, OD, D//4] -> [B, T//4, OD * D//4]
         out = out.contiguous().view(B, NT, OD*ND)
-        lens_out = (lens//2)//2
+        # NOTE (Huahuan): use torch.div() instead '//'
+        lens_out = torch.div(lens, 2, rounding_mode='floor')
+        lens_out = torch.div(lens_out, 2, rounding_mode='floor')
         return out, lens_out
 
 
@@ -529,13 +531,14 @@ class _LSTM(nn.Module):
     def forward(self, x: torch.Tensor, ilens: torch.Tensor, hidden=None):
         self.lstm.flatten_parameters()
 
-        packed_input = pack_padded_sequence(x, ilens.to("cpu"), batch_first=True)
+        packed_input = pack_padded_sequence(
+            x, ilens.to("cpu"), batch_first=True)
         packed_output, _ = self.lstm(packed_input, hidden)
         out, olens = pad_packed_sequence(packed_output, batch_first=True)
 
         return out, olens
 
-    
+
 class DeformTDNNlayer(nn.Module):
     def __init__(self, idim=120, hdim=640, dropout=0.5, kernel_size=5, dilation=1, padding=2, stride=1, bias=None, modulation=False, low_latency=False):
         """
@@ -550,9 +553,11 @@ class DeformTDNNlayer(nn.Module):
         self.padding = padding
         self.stride = stride
         self.zero_padding = nn.ZeroPad2d(padding)
-        self.conv = nn.Conv1d(idim, hdim, kernel_size=kernel_size, stride=kernel_size, bias=bias)
+        self.conv = nn.Conv1d(
+            idim, hdim, kernel_size=kernel_size, stride=kernel_size, bias=bias)
 
-        self.p_conv = nn.Conv1d(idim, kernel_size, kernel_size=5, padding=2, stride=stride)
+        self.p_conv = nn.Conv1d(
+            idim, kernel_size, kernel_size=5, padding=2, stride=stride)
         nn.init.constant_(self.p_conv.weight, 0)
         self.p_conv.register_backward_hook(self._set_lr)
 
@@ -563,7 +568,8 @@ class DeformTDNNlayer(nn.Module):
         setattr(self, "dropout", torch.nn.Dropout(dropout))
         if modulation:
             print("use modulation !")
-            self.m_conv = nn.Conv1d(idim, kernel_size, kernel_size=5, padding=2, stride=stride)
+            self.m_conv = nn.Conv1d(
+                idim, kernel_size, kernel_size=5, padding=2, stride=stride)
             nn.init.constant_(self.m_conv.weight, 0)
             self.m_conv.register_backward_hook(self._set_lr)
 
@@ -575,20 +581,19 @@ class DeformTDNNlayer(nn.Module):
     def forward(self, x):
         l = x.size(1)
         c = x.size(2)
-        x =x.transpose(1, 2)
+        x = x.transpose(1, 2)
         offset = self.p_conv(x)
-        
+
         if self.low_latency:
             zero = torch.zeros_like(offset)
             offset = torch.where(offset > 0, zero, offset)
-        
+
         if self.modulation:
             m = torch.sigmoid(self.m_conv(x))
 
         dtype = offset.data.type()
         ks = self.kernel_size
         N = offset.size(1)
-
 
         p = self._get_p(ks, l, c, offset, dtype, self.dilation)
         p = p.contiguous().permute(0, 2, 1)
@@ -602,7 +607,7 @@ class DeformTDNNlayer(nn.Module):
         x_q_l = self._get_x_q(x, q_l, N)
         x_q_r = self._get_x_q(x, q_r, N)
         x_offset = g_l.unsqueeze(dim=1) * x_q_l + \
-                   g_r.unsqueeze(dim=1) * x_q_r
+            g_r.unsqueeze(dim=1) * x_q_r
         if self.modulation:
             m = m.contiguous().permute(0, 2, 1)
             m = m.unsqueeze(dim=1)
@@ -619,7 +624,8 @@ class DeformTDNNlayer(nn.Module):
         return out
 
     def _get_p_n(self, N, dtype, dilation):
-        p_n = torch.arange(-(self.kernel_size-1)*dilation//2, (self.kernel_size-1)*dilation//2+1,dilation)
+        p_n = torch.arange(-(self.kernel_size-1)*dilation//2,
+                           (self.kernel_size-1)*dilation//2+1, dilation)
         p_n = p_n.view(1, N, 1).type(dtype)
 
         return p_n
@@ -630,7 +636,7 @@ class DeformTDNNlayer(nn.Module):
 
         return p_0.type(dtype)
 
-    def _get_p(self, ks, l, c, offset, dtype,dilation):
+    def _get_p(self, ks, l, c, offset, dtype, dilation):
         p_n = self._get_p_n(ks, dtype, dilation)
         p_0 = self._get_p_0(l, c, ks, dtype)
         p = p_0 + p_n + offset
@@ -641,7 +647,8 @@ class DeformTDNNlayer(nn.Module):
         c = x.size(1)
         x = x.contiguous().view(b, c, -1)
         index = q
-        index = index.contiguous().unsqueeze(dim=1).expand(-1, c, -1, -1).contiguous().view(b, c, -1)
+        index = index.contiguous().unsqueeze(
+            dim=1).expand(-1, c, -1, -1).contiguous().view(b, c, -1)
         x_offset = x.gather(dim=-1, index=index).contiguous().view(b, c, l, N)
 
         return x_offset
@@ -649,6 +656,7 @@ class DeformTDNNlayer(nn.Module):
     @staticmethod
     def _reshape_x_offset(x_offset, ks):
         b, c, l, N = x_offset.size()
-        x_offset = torch.cat([x_offset[..., s:s+ks].contiguous().view(b, c, l*ks) for s in range(0, N, ks)], dim=-1)
+        x_offset = torch.cat(
+            [x_offset[..., s:s+ks].contiguous().view(b, c, l*ks) for s in range(0, N, ks)], dim=-1)
         x_offset = x_offset.contiguous().view(b, c, l*ks)
         return x_offset

scripts/ctc-crf/_specaug.py
@@ -7,7 +7,7 @@ Modified into spec aug with masking by ratio by Huahuan Zheng (zhh20@mails.tsing
 
 from _layers import StackDelta, UnStackDelta
 from typing import Union, Sequence
-import utils
+import coreutils
 
 import torch
 import torch.nn as nn
@@ -171,7 +171,7 @@ class MaskTime(nn.Module):
                     _out = self.mask_by_batch(
                         spec[i*ch+j][None, :spec_length[i], :])
                     outs.append(_out)
-            out = utils.pad_list(outs, 0.0, dim=1)
+            out = coreutils.pad_list(outs, 0.0, dim=1)
             out = out.view(*org_size)
         return out
 
@@ -255,7 +255,7 @@ class TimeWarp(torch.nn.Module):
                 _y = time_warp(spec[i*ch:i*ch+ch][:, :spec_lengths[i], :],
                                window=int(spec_lengths[i]*self.window))
                 ys.append(_y)
-            y = utils.pad_list(ys, 0.0, dim=1)
+            y = coreutils.pad_list(ys, 0.0, dim=1)
 
         y = y.view(*org_size)
 

scripts/ctc-crf/calculate_logits.py
@@ -5,13 +5,14 @@ Author: Hongyu Xiang, Keyu An, Zheng Huahuan
 """
 
 import json
-import utils
+import coreutils
 import argparse
 import kaldiio
 import numpy as np
 from tqdm import tqdm
 from train import build_model
 from dataset import InferDataset
+from collections import OrderedDict
 
 from torch.utils.data.distributed import DistributedSampler
 from torch.utils.data import DataLoader
@@ -22,7 +23,7 @@ import torch.multiprocessing as mp
 
 def main(args):
     if not torch.cuda.is_available():
-        utils.highlight_msg("Using CPU.")
+        coreutils.highlight_msg("Using CPU")
         single_worker('cpu', args.nj, args)
         return None
 
@@ -31,8 +32,11 @@ def main(args):
     print(f"> Global number of GPUs: {args.world_size}")
     num_jobs = args.nj
     if num_jobs <= ngpus_per_node:
-        utils.highlight_msg(
-            f"Number of jobs (--nj={num_jobs}) is too small.\nUse only one GPU for avoiding errors.")
+        coreutils.highlight_msg(
+            [
+                f"Number of jobs (--nj={num_jobs}) is too small",
+                "Use only one GPU for avoiding errors"
+            ])
         single_worker("cuda:0", num_jobs, args)
         return None
 
@@ -45,7 +49,8 @@ def main(args):
         return None
     else:
         # This is a hack for non-divisible length of data to number of GPUs
-        utils.highlight_msg("Using hack to deal with undivisible data length.")
+        coreutils.highlight_msg(
+            "Using hack to deal with undivisible seq length")
         mp.spawn(main_worker, nprocs=ngpus_per_node,
                  args=(ngpus_per_node, args, num_jobs-1))
         single_worker("cuda:0", 1, args, len(inferset)-res)
@@ -84,16 +89,16 @@ def main_worker(gpu, ngpus_per_node, args, num_jobs):
 
     torch.cuda.set_device(args.gpu)
     model.cuda(args.gpu)
-    model.load_state_dict(torch.load(
-        args.resume, map_location=f"cuda:{args.gpu}"))
     model = torch.nn.parallel.DistributedDataParallel(
         model, device_ids=[args.gpu])
+
+    load_checkpoint(model, args.resume, loc=f"cuda:{args.gpu}")
     model.eval()
 
     if args.rank == 0:
         print("> Model built.")
         print("  Model size:{:.2f}M".format(
-            utils.count_parameters(model)/1e6))
+            coreutils.count_parameters(model)/1e6))
 
     cal_logit(model, testloader, args.gpu, local_writers)
 
@@ -119,13 +124,13 @@ def single_worker(device, num_jobs, args, idx_beg=0):
     model = build_model(args, configures, train=False)
 
     model = model.to(device)
-    model.load_state_dict(torch.load(
-        args.resume, map_location=device))
+    load_checkpoint(model, args.resume, loc=device)
+
     model.eval()
 
     print("> Model built.")
     print("  Model size:{:.2f}M".format(
-        utils.count_parameters(model)/1e6))
+        coreutils.count_parameters(model)/1e6))
 
     cal_logit(model, testloader, device, local_writers)
 
@@ -154,6 +159,30 @@ def cal_logit(model, testloader, device, local_writers):
         kaldiio.save_ark(writer, dict(result))
 
     return None
+
+
+def load_checkpoint(model, path_ckpt, loc='cpu'):
+
+    checkpoint = torch.load(path_ckpt, map_location=loc)
+    state_dict = OrderedDict()
+    if 'module.' != next(iter(checkpoint['model'].keys()))[:7]:
+        # old version
+        if isinstance(model, torch.nn.parallel.DistributedDataParallel):
+            for k, v in checkpoint['model'].items():
+                # add the 'module.' prefix
+                state_dict['module.'+k] = v
+        else:
+            state_dict = checkpoint['model']
+    elif isinstance(model, torch.nn.parallel.DistributedDataParallel):
+        for k, v in checkpoint['model'].items():
+            # remove the 'infer.'
+            state_dict[k.replace('infer.', '')] = v
+    else:
+        for k, v in checkpoint['model'].items():
+            # remove the 'module.'
+            state_dict[k.replace('module.infer.', '')] = v
+    model.load_state_dict(state_dict)
+    return model
 
 
 if __name__ == "__main__":

scripts/ctc-crf/dataset.py
@@ -7,9 +7,8 @@ Author: Hongyu Xiang, Keyu An, Zheng Huahuan (zhh20@mails.tsinghua.edu.cn)
 import os
 import kaldiio
 import h5py
-import utils
+import coreutils
 import pickle
-import numpy as np
 from kaldiio import ReadHelper
 from typing import Union, Tuple, Sequence
 
@@ -129,7 +128,7 @@ class sortedPadCollate():
                    for mat, label, weight in batch]
         batch_sorted = sorted(batches, key=lambda item: item[3], reverse=True)
 
-        mats = utils.pad_list([x[0] for x in batch_sorted])
+        mats = coreutils.pad_list([x[0] for x in batch_sorted])
 
         labels = torch.cat([x[1] for x in batch_sorted])
 

scripts/ctc-crf/scheduler.py
@@ -10,14 +10,47 @@ import math
 import torch
 import numpy as np
 from collections import OrderedDict
+from typing import Tuple, Iterable, Union
+
+from torch.distributed.optim import ZeroRedundancyOptimizer
 
 
-def SetupOptim(type_optim: str, paramlist, **kwargs) -> torch.optim.Optimizer:
-    return getattr(torch.optim, type_optim)(paramlist, **kwargs)
+def SetupOptim(type_optim: str, paramlist: Iterable[torch.nn.parameter.Parameter], use_zero: bool = False, **kwargs) -> Union[torch.optim.Optimizer, ZeroRedundancyOptimizer]:
+    """Setup the optimizer.
+
+    Args:
+        type_optim (str): name of optimizer, should be an attribute of `torch.optim`, like `Adam`, `SGD`.
+        paramlist (Iterable[Parameter]): a iterator or generator that returns parameters to be optimized.
+        use_zero (bool, default False): a flag to determinte whether use `ZeroRedundancyOptimizer` or not,
+            ref to https://pytorch.org/tutorials/recipes/zero_redundancy_optimizer.html, which is supported since
+            torch 1.8.0
+        **kwargs: any keyword arguments can be passed into optimizer initializatio.
+    
+    Return:
+        optimizer (torch.optim.Optimizer | ZeroRedundancyOptimizer)
+
+    Example:
+        >>> # With `use_zero=False`
+        >>> model = nn.Linear(3,4)
+        >>> optimizer = SetupOptim('Adam', model.parameters(), lr=1e-3, betas=(0.9,0.99))
+        >>> # With `use_zero=True`
+        >>> # ... (init of DDP)
+        >>> model = torch.nn.parallel.DistributedDataParallel(model)
+        >>> optimizer = SetupOptim('Adam', model.parameters(), lr=1e-3, betas=(0.9,0.99))
+    """
+    if not use_zero:
+        return getattr(torch.optim, type_optim)(paramlist, **kwargs)
+    else:
+        raise NotImplementedError
+        print("Using zero reduncdancy optimizer...")
+        # FIXME: This is still a experimental function in torch 1.9.0
+        zerooptimizer = ZeroRedundancyOptimizer(
+            params=paramlist, optim=getattr(torch.optim, type_optim), **kwargs)
+        return zerooptimizer
 
 
 class Scheduler(object):
-    def __init__(self, optimizer_configs, paramlist, reverse_metric_direc=False):
+    def __init__(self, optimizer_configs: dict, paramlist: Iterable[torch.nn.parameter.Parameter], reverse_metric_direc=False):
         super().__init__()
         self.optimizer = SetupOptim(
             optimizer_configs['type_optim'], paramlist, **optimizer_configs['kwargs'])
@@ -33,7 +66,7 @@ class Scheduler(object):
     def update_lr(self, *args, **kwargs):
         return None
 
-    def _adjust_lr_(self, lr):
+    def _adjust_lr_(self, lr: float):
         for param_group in self.optimizer.param_groups:
             param_group['lr'] = lr
 
@@ -46,7 +79,7 @@ class Scheduler(object):
                 output[name] = value
         return output
 
-    def load_state_dict(self, ckpt: OrderedDict, optim_only=False):
+    def load_state_dict(self, ckpt: OrderedDict, optim_only: bool = False):
         if optim_only:
             self.optimizer.load_state_dict(ckpt['optimizer'])
             return None
@@ -59,21 +92,23 @@ class Scheduler(object):
             else:
                 setattr(self, name, ckpt[name])
 
-    def impl_step(self, metric):
+    def impl_step(self, metric) -> Tuple[int, str]:
         raise NotImplementedError
 
-    def step(self, global_epoch, metric):
+    def step(self, global_epoch: int, metric) -> Tuple[int, str]:
         """Optimizer step
 
         Args:
             global_epoch (int): the global epoch (begins from 1)
             metric (obj): the metric for evaluate the performance
 
-        Returns:
-            int: choice of `[0, 1, 2]`, meaning
+        Returns: (state, info)
+            state (int): choice of `[0, 1, 2]`, meaning
                 0: continue training by the prior condition
                 1: continue training for metric is improving
                 2: stop training.
+            
+            info (str): information
         """
         if self.best_metric is None:
             self.best_metric = metric
@@ -102,6 +137,7 @@ class SchedulerEarlyStop(Scheduler):
     def impl_step(self, metric):
 
         state = 0
+        info = ''
         if self.epoch_cur <= self.epoch_min:
             if not (self._reverse_ ^ (metric < self.best_metric)):
                 self.best_metric = metric
@@ -114,21 +150,17 @@ class SchedulerEarlyStop(Scheduler):
             self.count_worse += 1
             if self.count_worse >= self.num_ahead:
                 lr = self.lr_cur
-                print("Validation metrics doesn't improve\nDecay the learning rate from {:.2e} to {:.2e}".format(
-                    lr, lr * self.gamma))
                 lr *= self.gamma
                 if lr < self.lr_stop:
-                    print("lr: {:.2e} < lr_stop: {:.2e}, terminate training.".format(
-                        lr, self.lr_stop))
                     state = 2
                 else:
                     self._adjust_lr_(lr)
                     self.count_worse = 0
 
-        print("Epoch: [{}@{}] | best={:.2f} | current={:.2f} | worse_count={} | lr={:.2e}".format(
-            self.epoch_cur, self.epoch_min, self.best_metric, metric, self.count_worse, self.lr_cur))
+        info = "Epoch: [{}@{}] | best={:.2f} | current={:.2f} | worse_count={} | lr={:.2e}".format(
+            self.epoch_cur, self.epoch_min, self.best_metric, metric, self.count_worse, self.lr_cur)
 
-        return state
+        return state, info
 
 
 class SchedulerFixedStop(Scheduler):
@@ -154,10 +186,10 @@ class SchedulerFixedStop(Scheduler):
 
         self.custom_update()
 
-        print("Epoch: [{}/{}] | best={:.2f} | current={:.2f} | lr={:.2e}".format(
-            self.epoch_cur, self.epoch_max, self.best_metric, metric, self.lr_cur))
+        info = "Epoch: [{}/{}] | best={:.2f} | current={:.2f} | lr={:.2e}".format(
+            self.epoch_cur, self.epoch_max, self.best_metric, metric, self.lr_cur)
 
-        return state
+        return state, info
 
 
 class SchedulerWarmupMileStone(SchedulerEarlyStop):
@@ -205,9 +237,9 @@ class SchedulerWarmupMileStone(SchedulerEarlyStop):
                 state = 1
             cur_lr = self.lr_cur
             self._adjust_lr_(cur_lr+self.lr_addon)
-            print("Epoch: [{}/{}] | best={:.2f} | current={:.2f} | lr={:.2e}".format(
-                self.epoch_cur, self.epoch_warmup, self.best_metric, metric, self.lr_cur))
-            return state
+            info = "Epoch: [{}/{}] | best={:.2f} | current={:.2f} | lr={:.2e}".format(
+                self.epoch_cur, self.epoch_warmup, self.best_metric, metric, self.lr_cur)
+            return state, info
         else:
             return super().impl_step(metric)
 
@@ -284,15 +316,14 @@ class SchedulerTransformerEarlyStop(SchedulerEarlyStop):
             if not (self._reverse_ ^ (metric < self.best_metric)):
                 self.best_metric = metric
 
-            print("Epoch: [{}] | best={:.2f} | current={:.2f} | lr={:.2e}".format(
-                self.epoch_cur, self.best_metric, metric, self.lr_cur))
-            return 0
+            return 0, "Epoch: [{}] | best={:.2f} | current={:.2f} | lr={:.2e}".format(
+                self.epoch_cur, self.best_metric, metric, self.lr_cur)
         else:
             lr0 = self.lr_cur
-            state = super().impl_step(metric)
+            states = super().impl_step(metric)
             lr1 = self.lr_cur
             self.lr_init *= lr1 / lr0
-            return state
+            return states
 
 
 class SchedulerIterAnnealing(SchedulerFixedStop):

scripts/ctc-crf/train.py
@@ -8,13 +8,14 @@ Differed from `train_dist.py`, this one supports read configurations from json f
 and is more non-hard-coding style.
 """
 
-import utils
+import coreutils
 import os
 import argparse
-import numpy as np
 import model as model_zoo
 import dataset as DataSet
 from collections import OrderedDict
+from ctc_crf import CTC_CRF_LOSS as CRFLoss
+from ctc_crf import WARP_CTC_LOSS as CTCLoss
 
 import torch
 import torch.nn as nn
@@ -28,14 +29,8 @@ import ctc_crf_base
 
 def main(args):
     if not torch.cuda.is_available():
-        utils.highlight_msg("CPU only training is unsupported.")
+        coreutils.highlight_msg("CPU only training is unsupported")
         return None
-
-    os.makedirs(args.dir+'/ckpt', exist_ok=True)
-    setattr(args, 'ckptpath', args.dir+'/ckpt')
-    if os.listdir(args.ckptpath) != [] and not args.debug and args.resume is None:
-        raise FileExistsError(
-            f"{args.ckptpath} is not empty! Refuse to run the experiment.")
 
     ngpus_per_node = torch.cuda.device_count()
     args.world_size = ngpus_per_node * args.world_size
@@ -43,7 +38,8 @@ def main(args):
     mp.spawn(main_worker, nprocs=ngpus_per_node, args=(ngpus_per_node, args))
 
 
-def main_worker(gpu, ngpus_per_node, args):
+def main_worker(gpu: int, ngpus_per_node: int, args: argparse.Namespace):
+    coreutils.SetRandomSeed(args.seed)
     args.gpu = gpu
 
     args.rank = args.rank * ngpus_per_node + gpu
@@ -55,11 +51,16 @@ def main_worker(gpu, ngpus_per_node, args):
 
     args.batch_size = args.batch_size // ngpus_per_node
 
-    print("> Data prepare")
+    if args.gpu == 0:
+        print("> Data prepare")
     if args.h5py:
         data_format = "hdf5"
-        utils.highlight_msg("H5py reading might cause error with Multi-GPUs.")
+        coreutils.highlight_msg(
+            "H5py reading might cause error with Multi-GPUs")
         Dataset = DataSet.SpeechDataset
+        if args.trset is None or args.devset is None:
+            raise FileNotFoundError(
+                "With '--hdf5' option, you must specify data location with '--trset' and '--devset'.")
     else:
         data_format = "pickle"
         Dataset = DataSet.SpeechDatasetPickle
@@ -72,7 +73,8 @@ def main_worker(gpu, ngpus_per_node, args):
 
     tr_set = Dataset(args.trset)
     test_set = Dataset(args.devset)
-    print("Data prepared.")
+    if args.gpu == 0:
+        print("  Data prepared.")
 
     train_sampler = DistributedSampler(tr_set)
     test_sampler = DistributedSampler(test_set)
@@ -92,18 +94,18 @@ def main_worker(gpu, ngpus_per_node, args):
         'log_train': ['epoch,loss,loss_real,net_lr,time'],
         'log_eval': ['loss_real,time']
     })
-    manager = utils.Manager(logger, build_model, args)
+    manager = coreutils.Manager(logger, build_model, args)
 
     # get GPU info
-    gpu_info = utils.gather_all_gpu_info(args.gpu)
+    gpu_info = coreutils.gather_all_gpu_info(args.gpu)
 
     if args.rank == 0:
         print("> Model built.")
-        print("Model size:{:.2f}M".format(
-            utils.count_parameters(manager.model)/1e6))
+        print("  Model size:{:.2f}M".format(
+            coreutils.count_parameters(manager.model)/1e6))
 
-        utils.gen_readme(args.dir+'/readme.md',
-                         model=manager.model, gpu_info=gpu_info)
+        coreutils.gen_readme(args.dir+'/readme.md',
+                             model=manager.model, gpu_info=gpu_info)
 
     # init ctc-crf, args.iscrf is set in build_model
     if args.iscrf:
@@ -117,20 +119,12 @@ def main_worker(gpu, ngpus_per_node, args):
         ctc_crf_base.release_env(gpus)
 
 
-class CAT_Model(nn.Module):
-    def __init__(self, NET=None, fn_loss='crf', lamb: float = 0.1, net_kwargs: dict = None):
+class AMTrainer(nn.Module):
+    def __init__(self, am: nn.Module, criterion: nn.Module):
         super().__init__()
-        if NET is None:
-            return None
 
-        self.infer = NET(**net_kwargs)
-
-        if fn_loss == "ctc":
-            self.loss_fn = utils.CTCLoss()
-        elif fn_loss == "crf":
-            self.loss_fn = utils.CRFLoss(lamb=lamb)
-        else:
-            raise ValueError(f"Unknown loss function: {fn_loss}")
+        self.infer = am
+        self.criterion = criterion
 
     def forward(self, logits, labels, input_lengths, label_lengths):
         labels = labels.cpu()
@@ -140,7 +134,7 @@ class CAT_Model(nn.Module):
         netout, lens_o = self.infer(logits, input_lengths)
         netout = torch.log_softmax(netout, dim=-1)
 
-        loss = self.loss_fn(netout, labels, lens_o.to(
+        loss = self.criterion(netout, labels, lens_o.to(
             torch.int32).cpu(), label_lengths)
 
         return loss
@@ -149,30 +143,39 @@ class CAT_Model(nn.Module):
 def build_model(args, configuration, train=True) -> nn.Module:
 
     netconfigs = configuration['net']
-    net_kwargs = netconfigs['kwargs']
+    net_kwargs = netconfigs['kwargs']   # type:dict
     net = getattr(model_zoo, netconfigs['type'])
 
+    am_model = net(**net_kwargs)    # type:nn.Module
     if not train:
-        infer_model = net(**net_kwargs)
-        return infer_model
+        return am_model
 
     if 'lossfn' not in netconfigs:
         lossfn = 'crf'
-        utils.highlight_msg(
-            "Warning: not specified \'lossfn\' in configuration.\nDefaultly set to \'crf\'")
+        coreutils.highlight_msg([
+            "Warning: not specified \'lossfn\' in configuration",
+            "Defaultly set to \'crf\'"
+        ])
     else:
         lossfn = netconfigs['lossfn']
 
-    if 'lamb' not in netconfigs:
-        lamb = 0.01
-        if lossfn == 'crf':
-            utils.highlight_msg(
-                "Warning: not specified \'lamb\' in configuration.\nDefaultly set to 0.01")
+    if lossfn == 'crf':
+        if 'lamb' not in netconfigs:
+            lamb = 0.01
+            coreutils.highlight_msg([
+                "Warning: not specified \'lamb\' in configuration",
+                "Defaultly set to 0.01"
+            ])
+        else:
+            lamb = netconfigs['lamb']
+        loss_fn = CRFLoss(lamb=lamb)
+    elif lossfn == "ctc":
+        loss_fn = CTCLoss()
     else:
-        lamb = netconfigs['lamb']
+        raise ValueError(f"Unknown loss function: {lossfn}")
 
     setattr(args, 'iscrf', lossfn == 'crf')
-    model = CAT_Model(net, lossfn, lamb, net_kwargs)
+    model = AMTrainer(am_model, loss_fn)
 
     torch.cuda.set_device(args.gpu)
     model.cuda(args.gpu)
@@ -182,63 +185,23 @@ def build_model(args, configuration, train=True) -> nn.Module:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="recognition argument")
-
-    parser.add_argument('--batch_size', default=256, type=int, metavar='N',
-                        help='mini-batch size (default: 256), this is the total '
-                        'batch size of all GPUs on the current node when '
-                        'using Distributed Data Parallel')
-
-    parser.add_argument("--seed", type=int, default=0,
-                        help="Manual seed.")
-
-    parser.add_argument("--resume", type=str, default=None,
-                        help="Path to location of checkpoint.")
-
-    parser.add_argument("--debug", action="store_true",
-                        help="Configure to debug settings, would overwrite most of the options.")
+    parser = coreutils.BasicDDPParser()
     parser.add_argument("--h5py", action="store_true",
                         help="Load data with H5py, defaultly use pickle (recommended).")
 
-    parser.add_argument("--config", type=str, default=None, metavar='PATH',
-                        help="Path to configuration file of training procedure.")
-
-    parser.add_argument("--data", type=str, default=None,
-                        help="Location of training/testing data.")
-    parser.add_argument("--trset", type=str, default=None,
-                        help="Location of training data. Default: <data>/[pickle|hdf5]/tr.[pickle|hdf5]")
-    parser.add_argument("--devset", type=str, default=None,
-                        help="Location of dev data. Default: <data>/[pickle|hdf5]/cv.[pickle|hdf5]")
-    parser.add_argument("--dir", type=str, default=None, metavar='PATH',
-                        help="Directory to save the log and model files.")
-    parser.add_argument("--grad-accum-fold", type=int, default=1,
-                        help="Utilize gradient accumulation for K times. Default: K=1")
-
-    parser.add_argument('-p', '--print-freq', default=10, type=int,
-                        metavar='N', help='print frequency (default: 10)')
-
-    parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
-                        help='number of data loading workers (default: 4)')
-    parser.add_argument('--rank', default=-1, type=int,
-                        help='node rank for distributed training')
-    parser.add_argument('--dist-url', default='tcp://127.0.0.1:13943', type=str,
-                        help='url used to set up distributed training')
-    parser.add_argument('--dist-backend', default='nccl', type=str,
-                        help='distributed backend')
-    parser.add_argument('--world-size', default=-1, type=int,
-                        help='number of nodes for distributed training')
-    parser.add_argument('--gpu', default=None, type=int,
-                        help='GPU id to use.')
-
     args = parser.parse_args()
 
-    SEED = args.seed
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed_all(SEED)
-    np.random.seed(SEED)
-    torch.backends.cudnn.deterministic = True
+    if not args.debug:
+        ckptpath = os.path.join(args.dir, 'ckpt')
+        os.makedirs(ckptpath, exist_ok=True)
+    else:
+        coreutils.highlight_msg("Debugging")
+        # This is a hack, we won't read/write anything in debug mode.
+        ckptpath = '/'
 
-    if args.debug:
-        utils.highlight_msg("Debugging.")
+    setattr(args, 'ckptpath', ckptpath)
+    if os.listdir(ckptpath) != [] and not args.debug and args.resume is None:
+        raise FileExistsError(
+            f"{args.ckptpath} is not empty! Refuse to run the experiment.")
 
     main(args)